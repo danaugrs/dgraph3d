344130814d954c7bf431f899ecb1db509fc03ddb
hijklmno <watson2272003@hotmail.com>
2014-01-11 01:40:18 -0800

    updated socket

--- updater/js/socket.js
+++ updater/js/socket.js
@@ -1,3 +1,11 @@
-function popup() {
-	window.open("about:blank");
+var http = require("http");
+
+function onRequest(request, response) {
+	console.log("Request received.");
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World");
+	response.end();
 }
+http.createServer(onRequest).listen(7777);
+
+console.log("Server has started.");
